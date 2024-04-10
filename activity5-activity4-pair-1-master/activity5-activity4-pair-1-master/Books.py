d8272cbda957f92478f8b500353fc811bc94f7ff
Austin Hendricks <112452064+AustinMH0@users.noreply.github.com>
2024-04-09 17:23:31 -0700

    Delete activity5-activity4-pair-1-master/activity5-activity4-pair-1-master directory
    Delete Directory. Fixing branch.

--- activity5-activity4-pair-1-master/activity5-activity4-pair-1-master/Books.py
+++
@@ -1,9 +0,0 @@
-class Books:
-
-  def __init__(self, title, author, ISBN, publisher, quantity,price):
-    self.title = title
-    self.author = author
-    self.ISBN = ISBN
-    self.publisher = publisher
-    self.quantity = quantity
-    self.price = price  
